aab866cba9f1c2e198a97d9a6e2bd6521c1c6a5a
Dmitriy <glovatskiy.dmitriy@gmail.com>
2017-02-23 18:01:25 +0200

    Add files via upload

---
+++ Primes/dev-cpp.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Dev-Cpp\MinGW64\bin\gcc.exe" "main.o" "prime.o" "-o" "primes.exe" "-LC:/Program Files (x86)/Dev-Cpp/MinGW64/lib" "-LC:/Program Files (x86)/Dev-Cpp/MinGW64/x86_64-w64-mingw32/lib" "-static-libgcc" 2>"dev-cpp-err.log"
---
+++ Primes/prime.h
@@ -0,0 +1,26 @@
+#ifndef prime_h
+#define prime_h
+
+
+/*
+**	Sieve for non-prime numbers
+*/
+void sieve ( int size, char numbers[size] );
+
+/*
+**	Print on the screen prime-numbers from the sieved array
+*/
+int prime_list(int size, char numbers[size] );
+
+
+/*
+**	Print prime-numbers in the file
+*/
+int prime_file(int size, char numbers[size] );
+
+#endif
+
+
+/*
+	
+*/
